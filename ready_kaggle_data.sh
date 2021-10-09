a28efdaa88f8e3ca1ce2a4f2f0ecf63f28008168
KtechB <43807089+KtechB@users.noreply.github.com>
2021-10-09 16:35:13 +0900

    add input check

--- ready_kaggle_data.sh
+++ ready_kaggle_data.sh
@@ -6,7 +6,16 @@ if [ $# -ne 1 ]; then
 fi
 
 COMP_NAME=$1
+SEARCH_COUNT=$(kaggle competitions list -s $COMP_NAME|grep $COMP_NAME -c)
+
+if [ $SEARCH_COUNT -ne 1 ]; then
+  echo "error:competition:$COMP_NAME is not found" 1>&2
+  exit 1
+fi
 
 mkdir -p $COMP_NAME/input
 kaggle competitions download -c $COMP_NAME -p $COMP_NAME/input
-unzip $COMP_NAME/input/$COMP_NAME.zip -d $COMP_NAME/input$COMP_NAME
+unzip $COMP_NAME/input/$COMP_NAME.zip -d $COMP_NAME/input/$COMP_NAME
+rm $COMP_NAME/input/$COMP_NAME.zip
+mkdir $COMP_NAME/src
+
